aad26695c0b83a27716c4fd8283f708e0ef72a9c
Redouane <Redouane@Redouane.local>
2020-12-06 01:24:45 +0100

    form fiche inscription

--- client/src/components/FicheInscription/index.js
+++ client/src/components/FicheInscription/index.js
@@ -9,16 +9,16 @@ const FicheInscription = () => {
   const [date, setDate] = useState();
   const [email, setEmail] = useState("");
   const [lieu, setLieu] = useState("");
-  const [subject, setSubject] = useState("");
-  const [message, setMessage] = useState("");
+  const [adresse, setAdresse] = useState("");
+  const [tel,setTel]=useState();
+  const[nationality,setNationality]= useState("");
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const response = await axios.post("/mail", {
       name,
-      email,
-      subject,
-      message,
+      email
     });
     console.log("response", response);
   };
@@ -40,29 +40,35 @@ const FicheInscription = () => {
     setDate(date);
     // console.log(e.target.value)
   };
-  
+
+  const HandleChangeTel = (e) => {
+    const tel = e.target.value;
+    setTel(tel);
+    // console.log(e.target.value)
+  };
+
 
   const HandleChangeLieuNaissance = (e) => {
     const lieu = e.target.value;
     setLieu(lieu);
     // console.log(e.target.value)
   };
-
+  //HandleChangeAdresse
+  const HandleChangeAdresse = (e) => {
+    const adresse = e.target.value;
+    setAdresse(adresse);
+    // console.log(e.target.value)
+  };
   const HandleChangeEmail = (e) => {
     const email = e.target.value;
     setEmail(email);
     // console.log(e.target.value)
   };
 
-  const HandleChangeSubject = (e) => {
-    const subject = e.target.value;
-    setSubject(subject);
-    // console.log(e.target.value)
-  };
 
-  const HandleChangeMessage = (e) => {
-    const message = e.target.value;
-    setMessage(message);
+  const HandleChangeNationality = (e) => {
+    const nationality = e.target.value;
+    setNationality(nationality);
     // console.log(e.target.value)
   };
   return (
@@ -75,30 +81,31 @@ const FicheInscription = () => {
         type="submit"
       >
         <div>
-          <Form.Group controlId="formBasicPassword" onChange={HandleChangeName}>
+          <Form.Group controlId="formBasicName" onChange={HandleChangeName}>
             <Form.Control type="text" placeholder="Name" />
           </Form.Group>
 
           <Form.Group
-            controlId="formBasicPassword"
+            controlId="formBasicPrenom"
             onChange={HandleChangePrenom}
           >
             <Form.Control type="text" placeholder="Prenom" />
           </Form.Group>
         </div>
         <div>
-          <Form.Group controlId="formBasicPassword" onChange={HandleChangeDAte}>
+          <Form.Group controlId="formBasicDate" onChange={HandleChangeDAte}>
             <Form.Control type="date" placeholder="Date" />
           </Form.Group>
 
           <Form.Group
-            controlId="formBasicPassword"
+            controlId="formBasicLieu"
             onChange={HandleChangeLieuNaissance}
           >
             <Form.Control type="text" placeholder="Lieu de naissance" />
           </Form.Group>
         </div>
 
+        <div>
         <Form.Group
           controlId="formBasicEmail"
           onChange={HandleChangeEmail}
@@ -110,20 +117,37 @@ const FicheInscription = () => {
         </Form.Group>
 
         <Form.Group
-          controlId="formBasicEmail"
-          onChange={HandleChangeSubject}
+          controlId="formBasicTel"
+          onChange={HandleChangeTel}
+          required
+          type="tel"
+          value={tel}
+        >
+          <Form.Control type="text" placeholder="Tel:" />
+        </Form.Group>
+        </div>
+        <div>
+        <Form.Group
+          controlId="formBasicNationality"
+          onChange={HandleChangeNationality}
           required
           type="text"
-          value={subject}
+          value={nationality}
         >
-          <Form.Control type="text" placeholder="Objet" />
+          <Form.Control type="text" placeholder="Nationalité" />
         </Form.Group>
+
         <Form.Group
-          controlId="exampleForm.ControlTextarea1"
-          onChange={HandleChangeMessage}
+          controlId="formBasicAdresse"
+          onChange={HandleChangeAdresse}
+          required
+          type="tel"
+          value={adresse}
         >
-          <Form.Control className="textarea_contact" as="textarea" rows={4} />
+          <Form.Control type="text" placeholder="Adresse" />
         </Form.Group>
+        </div>
+
         <Button variant="primary" type="submit">
           Envoyer
         </Button>
