6bd8e4a2e0f2e9013b75766efcf77650d1dd3e46
Rodoudou <amrani.redouane@gmail.com>
2021-01-04 16:21:00 +0100

    centrer h1 page horaire

--- client/src/containers/HorairesActivites/index.js
+++ client/src/containers/HorairesActivites/index.js
@@ -5,13 +5,13 @@ import titles from '../../titres.json';
 const Horaires = () => {
 
   return (
-    <>
+    <div className="horaire-cards-content">
           <h1>{titles.horaires} </h1>
     <div className="myCard">
       <MyCard/>
     </div>
 
-    </>
+    </div>
   );
 };
 
