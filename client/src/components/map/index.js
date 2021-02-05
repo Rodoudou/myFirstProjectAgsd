9a26bc40bc956e625e276f7a0a13ad724e34a76e
Rodoudou <amrani.redouane@gmail.com>
2021-02-05 01:39:42 +0100

    last commit

--- client/src/components/map/index.js
+++ client/src/components/map/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import GoogleMapReact from "google-map-react";
-
+import {GOOGLE_KEY_API} from '../../Key_api';
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class Map extends Component {
@@ -17,7 +17,7 @@ class Map extends Component {
       // Important! Always set the container height explicitly
       <div style={{ height: "400px", width: "500px" }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: `AIzaSyAKi235ij6PAtgeYRLnq-s7zSj6aI2BvWk` }}
+          bootstrapURLKeys={{ key: GOOGLE_KEY_API }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
